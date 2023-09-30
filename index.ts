a3a36df1419edb865d02505b7a9761412d58e069
Lucian Bellini <lucianbellini1@gmail.com>
2023-09-30 14:25:13 -0300

    30-09

---
+++ index.ts
@@ -0,0 +1,7 @@
+// Sistema de banco de horas
+
+const somaHorasExtras = (salario: number, valorHorasExtras: number): number => {
+  return salario + valorHorasExtras
+}
+
+console.log(somaHorasExtras(2000, 500))
